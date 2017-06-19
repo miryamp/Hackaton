dfe377e834c83d7d2d1814e0a81d985474cab39c
Miryam <miryampeleg@gmail.com>
2017-06-19 16:18:28 +0300

    Added methods to interface.

--- Comeet/Common/IDbHandler.cs
+++ Comeet/Common/IDbHandler.cs
@@ -6,7 +6,18 @@ using System.Threading.Tasks;
 
 namespace Common
 {
-    interface IDbHandler
+    public interface IDbHandler
     {
+        User GetUser(Guid userId);
+
+        IEnumerable<Tag> GetTagsByUser(Guid userId);
+
+        IEnumerable<User> GetUsersByTags(IEnumerable<Tag> tags);
+
+        IEnumerable<User> GetUsersByString(string str);
+
+        Tag GetTag(string tagText);
+
+        IEnumerable<Tag> GetAllTagsInCategory(string categoryName);
     }
 }
